56a13775f126292ae8b6b5e2a140501111d491a1
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-11-04 16:53:33 +0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/seqjuq2ddn1e3rut?svg=true)](https://ci.appveyor.com/project/PershikovAlex/unit4-1)
---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,42 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryTest {
+    @BeforeEach
+    void SetUp() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
+
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    void shouldTest() {
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        SelenideElement form = $(".form");
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=name] input").setValue("Бобровский Павель");
+        $("[data-test-id=phone] input").setValue("+79222141414");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id='notification']").shouldBe(Condition.hidden);
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(11));
+    }
+}
