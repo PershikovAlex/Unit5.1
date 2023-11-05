1be8285655c5bfab5ef68fafc44060dcf676c7f8
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-11-05 21:29:51 +0500

    Add DataGenerator and change CardDeliveryTest

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,7 +1,6 @@
 package ru.netology.data;
 
 import com.github.javafaker.Faker;
-import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,38 +8,32 @@ import java.util.Locale;
 import java.util.Random;
 
 public class DataGenerator {
-    private DataGenerator(){}
+    private static final Faker faker = new Faker(new Locale("ru"));
+    private static Random random = new Random();
+    private static String[] cities = {"Москва", "Санкт-Петербург", "Казань", "Екатеринбург", "Тюмень"};
 
-    public static String generateName(String locale){
-        var faker = new Faker(new Locale(locale));
-        return faker.name().lastName() + " " + faker.name().firstName();
-    }
-
-    public static String generatePhone(String locale) {
-        var faker = new Faker(new Locale(locale));
-        return faker.phoneNumber().phoneNumber();
+    private DataGenerator() {
     }
 
     public static String generateCity() {
-        var cities = new String[] {"Москва", "Санкт-Петербург", "Екатеринбург", "Казань", "Тюмень", "Иркутск"};
-        return cities[new Random().nextInt(cities.length)];
+        int index = random.nextInt(cities.length);
+        return cities[index];
     }
 
-    public static String generateDate(int addDays) { // статичный метод
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-    public static class Registration {
-        private Registration() {
-        }
-        public static UserInfo generateUser(String locale) {
-            return new UserInfo(generateName(locale), generatePhone(locale), generateCity());
-        }
+    public static String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
     }
 
-    @Value
-    public static class UserInfo {
-        String name;
-        String phone;
-        String city;
+    public static String generateFullName() {
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone() {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static String generatePhoneNumber() {
+        return faker.numerify("+7 (###) ### ## ##");
     }
 }
--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -2,17 +2,13 @@ package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import lombok.var;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,29 +16,31 @@ import static com.codeborne.selenide.Selenide.open;
 class CardDeliveryTest {
     @BeforeEach
     void SetUp() {
-        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
     }
 
-    public String generateDate(long addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
-    }
 
     @Test
     void shouldTest() {
-        String planningDate = generateDate(5, "dd.MM.yyyy");
-        SelenideElement form = $(".form");
-        var validUser = DataGenerator.Registration.generateUser("ru");
-        var days = 10;
-        var firstDate = DataGenerator.generateDate(days);
-        
-        $("[data-test-id=city] input").setValue("Казань");
-        $("[data-test-id='date'] .input__control").doubleClick().sendKeys(planningDate);
-        $("[data-test-id=name] input").setValue("Бобровский Павель");
-        $("[data-test-id=phone] input").setValue("+79222141414");
+        String city = DataGenerator.generateCity();
+        String firstDate = DataGenerator.generateDate(5, "dd.MM.yyyy");
+        String secondDate = DataGenerator.generateDate(7, "dd.MM.yyyy");
+        String fullName = DataGenerator.generateFullName();
+        String phoneNumber = DataGenerator.generatePhoneNumber();
+
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(firstDate);
+        $("[data-test-id=name] input").setValue(fullName);
+        $("[data-test-id=phone] input").setValue(phoneNumber);
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $("[data-test-id='notification']").shouldBe(Condition.hidden);
-        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(11));
+        $(".notification__content").shouldBe(Condition.hidden);
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstDate));
+        $(".button").click();
+        $("[data-test-id=date] input").doubleClick().sendKeys(secondDate);
+        $(".button").click();
+        $(".button__text").click();
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondDate));
     }
 }
